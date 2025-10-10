5e15e49e4668de5f03e11f15cec7592c0c30cc56
raeperd <raeperd117@gmail.com>
2025-10-10 23:24:21 +0900

    test: Add TDD tests for POST /api/users endpoint

---
+++ user_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"testing"
+)
+
+func TestPostUsers(t *testing.T) {
+	t.Parallel()
+
+	testcases := map[string]UserPostRequestBody{
+		"username required": {
+			Username: "",
+			Email:    "test@test.com",
+			Password: "test",
+		},
+		"email required": {
+			Username: "test",
+			Email:    "",
+			Password: "test",
+		},
+		"password required": {
+			Username: "test",
+			Email:    "test@test.com",
+			Password: "",
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			res := httpPostUsers(t, tc)
+			testEqual(t, http.StatusUnprocessableEntity, res.StatusCode)
+
+			t.Cleanup(func() { _ = res.Body.Close() })
+		})
+	}
+}
+
+func httpPostUsers(t *testing.T, request UserPostRequestBody) *http.Response {
+	t.Helper()
+
+	body, err := json.Marshal(UserWrapper[UserPostRequestBody]{User: request})
+	testNil(t, err)
+
+	res, err := http.Post(endpoint+"/api/users", "application/json", bytes.NewBuffer(body))
+	testNil(t, err)
+
+	return res
+}
+
+type UserWrapper[T UserPostRequestBody | UserResponseBody] struct {
+	User T `json:"user"`
+}
+
+type UserPostRequestBody struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserResponseBody struct {
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+	Username string `json:"username"`
+	Bio      string `json:"bio"`
+	Image    string `json:"image"`
+}
