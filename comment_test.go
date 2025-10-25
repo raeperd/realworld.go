c90ec7edda8bf933fa93a0e89fd131c335d75db7
raeperd <raeperd117@gmail.com>
2025-10-25 12:02:06 +0900

    test: add failing test for GET /api/articles/:slug/comments

--- comment_test.go
+++ comment_test.go
@@ -194,3 +194,101 @@ type CommentResponse struct {
 	Body      string        `json:"body"`
 	Author    AuthorProfile `json:"author"`
 }
+
+func TestGetArticlesSlugComments_Success(t *testing.T) {
+	t.Parallel()
+
+	// Setup: Create a user and get token
+	unique := fmt.Sprintf("%d", time.Now().UnixNano())
+	username := "get_comments_author_" + unique
+	email := fmt.Sprintf("get_comments_%s@example.com", unique)
+
+	userReq := UserPostRequestBody{
+		Username: username,
+		Email:    email,
+		Password: "testpass123",
+	}
+	userRes := httpPostUsers(t, userReq)
+	test.Equal(t, http.StatusCreated, userRes.StatusCode)
+	t.Cleanup(func() { _ = userRes.Body.Close() })
+
+	var userResponse UserResponseBody
+	test.Nil(t, json.NewDecoder(userRes.Body).Decode(&userResponse))
+	token := userResponse.Token
+
+	// Create an article
+	articleReq := ArticlePostRequestBody{
+		Article: ArticlePostRequest{
+			Title:       "Article for Get Comments " + unique,
+			Description: "Test article",
+			Body:        "Article body",
+			TagList:     []string{"test"},
+		},
+	}
+
+	articleRes := httpPostArticles(t, articleReq, token)
+	test.Equal(t, http.StatusCreated, articleRes.StatusCode)
+	t.Cleanup(func() { _ = articleRes.Body.Close() })
+
+	var articleResponse ArticleResponseBody
+	test.Nil(t, json.NewDecoder(articleRes.Body).Decode(&articleResponse))
+	slug := articleResponse.Article.Slug
+
+	// Create multiple comments
+	comment1Req := CommentPostRequestBody{
+		Comment: CommentPostRequest{
+			Body: "First comment",
+		},
+	}
+	comment1Res := httpPostArticlesSlugComments(t, slug, comment1Req, token)
+	test.Equal(t, http.StatusCreated, comment1Res.StatusCode)
+	t.Cleanup(func() { _ = comment1Res.Body.Close() })
+
+	comment2Req := CommentPostRequestBody{
+		Comment: CommentPostRequest{
+			Body: "Second comment",
+		},
+	}
+	comment2Res := httpPostArticlesSlugComments(t, slug, comment2Req, token)
+	test.Equal(t, http.StatusCreated, comment2Res.StatusCode)
+	t.Cleanup(func() { _ = comment2Res.Body.Close() })
+
+	// Test: GET /api/articles/:slug/comments
+	res := httpGetArticlesSlugComments(t, slug, "")
+	test.Equal(t, http.StatusOK, res.StatusCode)
+	t.Cleanup(func() { _ = res.Body.Close() })
+
+	// Verify response structure and content
+	var response CommentsResponseBody
+	test.Nil(t, json.NewDecoder(res.Body).Decode(&response))
+	test.Equal(t, 2, len(response.Comments))
+
+	// Comments should be ordered by created_at DESC (most recent first)
+	test.Equal(t, "Second comment", response.Comments[0].Body)
+	test.Equal(t, "First comment", response.Comments[1].Body)
+	test.Equal(t, username, response.Comments[0].Author.Username)
+	test.Equal(t, username, response.Comments[1].Author.Username)
+	test.Equal(t, false, response.Comments[0].Author.Following)
+	test.NotNil(t, response.Comments[0].ID)
+	test.NotNil(t, response.Comments[0].CreatedAt)
+	test.NotNil(t, response.Comments[0].UpdatedAt)
+}
+
+func httpGetArticlesSlugComments(t *testing.T, slug string, token string) *http.Response {
+	t.Helper()
+
+	req, err := http.NewRequest(http.MethodGet, endpoint+"/api/articles/"+slug+"/comments", nil)
+	test.Nil(t, err)
+	if token != "" {
+		req.Header.Set("Authorization", "Token "+token)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	test.Nil(t, err)
+
+	return res
+}
+
+type CommentsResponseBody struct {
+	Comments []CommentResponse `json:"comments"`
+}
